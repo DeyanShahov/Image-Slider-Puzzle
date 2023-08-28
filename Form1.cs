52038e9cb63b85fe24ea6837d674b3b1128578f7
redfoxbg <redfoxbg@gmail.com>
2023-08-29 00:23:06 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace Image_Slider_Puzzle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFileEvent(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
